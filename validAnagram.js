e148c344bb0b7e028b2732e6ef0da2f3f8a48cfa
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-02-21 15:27:54 +0100

    add working solution

--- validAnagram.js
+++ validAnagram.js
@@ -10,12 +10,12 @@ function validAnagram(str1, str2){
   for(let char of str2){
       frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
   }
-  console.log(frequencyCounter1)
-  console.log(frequencyCounter2)
-  
+  for(let key in frequencyCounter1){
+      if((frequencyCounter1[key]) !== (frequencyCounter2[key])){
+        return false;
+        }
+  }
+  return true;
 }
 
-let str1 = 'testString1';
-let str2 = 'testString2';
-
-console.log(validAnagram(str1, str2));
+validAnagram('eleven plus two', 'twelve plus one');
