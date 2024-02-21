23be76e1063a22e7e386e2bee2b04fa2a6971a8d
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-02-21 14:54:41 +0100

    add two for loops to iterate through both given strings

--- validAnagram.js
+++ validAnagram.js
@@ -1,4 +1,21 @@
 function validAnagram(str1, str2){
-    // add whatever parameters you deem necessary - good luck!
-    if(str1.length != str2.length) return false;
+  if(str1.length !== str2.length){
+      return false;
   }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for(let char of str1){
+      frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+  }
+  for(let char of str2){
+      frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
+  }
+  console.log(frequencyCounter1)
+  console.log(frequencyCounter2)
+  
+}
+
+let str1 = 'testString1';
+let str2 = 'testString2';
+
+console.log(validAnagram(str1, str2));
