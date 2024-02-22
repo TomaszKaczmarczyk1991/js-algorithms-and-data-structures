dffe3882142817aba6f565d62603e911c2a334b9
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-02-22 22:09:17 +0100

    add working solution for countUniqueValues exercise

---
+++ countUniqueValues.js
@@ -0,0 +1,13 @@
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+    let counter = 1;
+      
+  for(let i = 0; i < arr.length-1; i++) {
+      if(arr[i] !== arr[i+1]){
+      counter++;
+      }
+    }
+    return counter;
+  }
+  
+  countUniqueValues([1,1,1,1,1,1,1,1,3]);  
