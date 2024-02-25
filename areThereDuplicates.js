240edd1ab61b436eaa6c959fca7d33dc4e734e7a
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-02-25 15:06:15 +0100

    add areThereDuplicates.js

---
+++ areThereDuplicates.js
@@ -0,0 +1,5 @@
+// Coding Exercise 4: Frequency Counter / Multiple Pointers - areThereDuplicates (OPTIONAL!!!)
+
+function areThereDuplicates() {
+    // good luck. (supply any arguments you deem necessary.)
+  }
