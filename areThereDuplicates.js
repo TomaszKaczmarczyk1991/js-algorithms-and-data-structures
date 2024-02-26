2c8151c4d4e18b6e2357aa237b489b77f4e7d92e
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-02-27 00:41:55 +0100

    add working solution

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -1,9 +1,17 @@
 // Coding Exercise 4: Frequency Counter / Multiple Pointers - areThereDuplicates (OPTIONAL!!!)
 
 function areThereDuplicates(...arg) {
-  const obj = {...arg};
-  console.log(obj);
-  return true;
-}
+  const arr = [...arg];  
+  let seen = {};
+    
+  for (let i = 0; i < arr.length; i++) {
+    if (seen[arr[i]]) {
+      return true;
+      } else {
+          seen[arr[i]] = true;
+      }
+  }
+  return false;
+}  
 
-console.log(areThereDuplicates(1, 'test', {author: "Ray Bradbury", title: "Fahrenheit 451"}));
+console.log(areThereDuplicates(1, 'test', {author: "Ray Bradbury", title: "Fahrenheit 451"}, 'test'));
