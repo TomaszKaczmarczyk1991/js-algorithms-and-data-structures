db97fb8655c765faf7bce38399bf4151018afed8
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-02-28 15:52:42 +0100

    first steps...

--- averagePair.js
+++ averagePair.js
@@ -1,3 +1,18 @@
-function averagePair(){
-    // add whatever parameters you deem necessary - good luck!
+function averagePair(arr, average){
+    let first = 0;
+    let last = arr.length - 1;
+    
+  while(first < last){
+    const avg = (arr[first] + arr[last]) / 2;
+    if(average === avg) return true;
+    
+    return true;
+  }
+  return false;
 }
+
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8));
+
+//  i
+// [1,3,3,5,6,7,10,12,19],8
+//    j
