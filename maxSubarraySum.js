f267a246808ab7519aed4077db95faf1142846ce
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-03-03 16:09:12 +0100

    add first if statement - edge case

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -1,3 +1,6 @@
-function maxSubarraySum(){
-    // add whatever parameters you deem necessary - good luck!
-  }
+function maxSubarraySum(arr, num){
+  if(num > arr.length) return null;
+  
+}
+
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)); // 5
