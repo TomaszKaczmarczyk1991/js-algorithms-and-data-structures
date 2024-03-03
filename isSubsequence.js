c389f84b2ba23f3034cfef832235eed7f4a85aaf
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-03-03 15:43:35 +0100

    add working solution

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,31 +1,13 @@
-// COMMENT SECTION:
-// end for today, i made a big mess here.
-// it's late and i am exhausted by my 1 year old son ;)
-// 
-
-function isSubsequence(string1, string2) {
-  let str1 = string1.split('');
-  let str2 = string2.split('');
-  
-  let i = 0;
-  let j = 0;
- 
-  
-  while(str2.length === 0){
-    
-    if(str1.length === 0) return true;
-    else if(str2.length === 0) return false;
-
-    else if(str1[i] !== str2[j]) j++;
-    else{
-        console.log(str1);
-        str1.splice(0,1);
-        console.log(j)
-        j++;
+function isSubsequence(str1, str2) {
+    var i = 0;
+    var j = 0;
+    if (!str1) return true;
+    while (j < str2.length) {
+      if (str2[j] === str1[i]) i++;
+      if (i === str1.length) return true;
+      j++;
     }
+    return false;
   }
-}
 
-
-
-console.log(isSubsequence('abc', 'abc'));
+console.log(isSubsequence('acdb', 'dacbd'));
