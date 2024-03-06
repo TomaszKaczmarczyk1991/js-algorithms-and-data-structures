394af6d1ececa699ba468cb8587982226c30374e
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-03-06 20:46:10 +0100

    add solution

--- productOfArray.js
+++ productOfArray.js
@@ -4,6 +4,11 @@
 // productOfArray([1,2,3]) // 6
 // productOfArray([1,2,3,10]) // 60
 
-function productOfArray(){
+function productOfArray(arr){
+    if(arr.length === 0) return undefined;
+    if(arr.length === 1) return arr[0];
+    return arr[0] * productOfArray(arr.slice(1));
     
 }
+
+console.log(productOfArray([3,5,4]));
