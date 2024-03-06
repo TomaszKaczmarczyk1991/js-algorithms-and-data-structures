3149ae546007053a0196c5ad883a66feb10018cd
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-03-06 20:24:21 +0100

    add working code

--- factorial.js
+++ factorial.js
@@ -3,7 +3,9 @@
 // factorial(4) // 24
 // factorial(7) // 5040
 
-function factorial(){
-   
-}
+function factorial(num){
+    if(num === 0) return 1;
+    return num * factorial(num - 1);
+ }
 
+console.log(factorial(7));
