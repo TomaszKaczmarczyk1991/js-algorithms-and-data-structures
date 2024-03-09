d54a82697d7f0ebdc75616509c597884dc5a7340
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-03-10 00:15:50 +0100

    add solution

--- fib.js
+++ fib.js
@@ -8,6 +8,11 @@
 // fib(28) // 317811
 // fib(35) // 9227465
 
-function fib(){
-    // add whatever parameters you deem necessary - good luck!  
+function fib(num){
+  if(num === 0) return null;
+  if(num === 1) return 1;  
+  
+  return fib(num - 1) + fib(num - 2);
   }
+
+console.log(fib(5));
