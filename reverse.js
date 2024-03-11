09f026e1dbd697b2409af05cb6550b078df04ec2
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-03-11 01:56:21 +0100

    add first idea

--- reverse.js
+++ reverse.js
@@ -1,6 +1,15 @@
-function reverse(){
-    // add whatever parameters you deem necessary - good luck!
-  }
+function reverse(str){
+  let string = Array.from(str);
+  // console.log(string) // ['a', 'w', 'e', 's', 'o', 'm', 'e']
+  let result = [];
+  let first = string[0];
+  let last = string[string.length - 1];
+
+  string.pop(last);
+  result.push(last);
   
-  // reverse('awesome') // 'emosewa'
-  // reverse('rithmschool') // 'loohcsmhtir'
+// remember that strings are immutable in js!
+console.log(result)
+}
+  
+console.log(reverse('awesome')); // 'emosewa'
