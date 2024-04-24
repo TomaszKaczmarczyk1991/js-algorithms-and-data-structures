ed13e1d3f37602c7756ecd8120230af775c7867d
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-04-24 14:32:58 +0200

    add naive solution (indexOf)

--- linearSearch.js
+++ linearSearch.js
@@ -1,3 +1,6 @@
-function linearSearch(){
-    // add whatever parameters you deem necessary - good luck!
+function linearSearch(arr, index){
+    return arr.indexOf(index);
   }
+
+
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4)); // 5
