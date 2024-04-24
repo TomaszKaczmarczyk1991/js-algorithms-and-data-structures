be589c408b9bcd05e18094738b489b851ee0eacf
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-04-24 14:23:01 +0200

    add someRecursive.js and linearSearch.js

--- someRecursive.js
+++ someRecursive.js
@@ -1,3 +1,7 @@
+// Write a recursive function called someRecursive which accepts an array and a callback.
+// The function returns true if a single value in the array returns true when passed
+// to the callback. Otherwise it returns false.
+
 // SAMPLE INPUT / OUTPUT
 // const isOdd = val => val % 2 !== 0;
 
