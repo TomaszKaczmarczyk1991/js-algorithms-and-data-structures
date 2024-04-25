6b877f303f2ac58a240853439c9745bf6bf2bc7e
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-04-25 21:59:44 +0200

    change if statements, add ternary operator

--- binarySearch.js
+++ binarySearch.js
@@ -4,18 +4,11 @@ function binarySearch(arr, val){
   let midPointer = Math.floor((leftPointer + rightPointer) / 2);
     
     while(arr[midPointer] !== val && leftPointer <= rightPointer){
-        if(val < arr[midPointer]) {
-          rightPointer = midPointer - 1;
-        } else {
-          leftPointer = midPointer + 1;
-        }
-        midPointer = Math.floor((leftPointer + rightPointer) / 2);
+      if(val < arr[midPointer]) rightPointer = midPointer - 1;
+      else leftPointer = midPointer + 1;
+      midPointer = Math.floor((leftPointer + rightPointer) / 2);
       }
-      if(val === arr[midPointer]){
-        return midPointer;
-      } else {
-        return -1;
-      }
+      return val === arr[midPointer] ? midPointer : -1;
   }
 
-  console.log(binarySearch([1,2,3,4],6)) // 4
+  console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11],6)) // 4
