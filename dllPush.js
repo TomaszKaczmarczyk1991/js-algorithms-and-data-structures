40243620ef616c5e433b3f6d63cf03ebc8f02db5
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-05-05 23:10:01 +0200

    add solution

--- dllPush.js
+++ dllPush.js
@@ -7,11 +7,37 @@ class Node{
 }
 
 class DoublyLinkedList{
-    constructor(val){
-        this.val = val
-        this.next = null;      
+    constructor(){
+        this.head = null;
+        this.next = null;    
+        this.length = 0;  
     }
-    push(){
+    push(val){
+        var newNode = new Node(val);
+        if(this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+            } else {
+                this.tail.next = newNode;
+                newNode.prev = this.tail;
+                this.tail = newNode;
+            }
+            this.length++;
+            return this;
     }
-    
 }
+
+
+// New DLL
+
+let list = new DoublyLinkedList();
+console.log(list); // { val: undefined, next: null }
+
+list.push(303);
+console.log(list);
+
+list.push(808);
+console.log(list);
+
+list.push(909);
+console.log(list);
