5dac9567cc9bc222f0a021d3edb0613e7c8b2dfd
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-05-13 13:40:07 +0200

    add solutions

--- dllGet.js
+++ dllGet.js
@@ -27,26 +27,48 @@ class DoublyLinkedList{
     }
     get(index){
         if(index < 0 || index >= this.length) return null;
-        var count = 0;
-        var current = this.head;
+        if(index <= this.length/2){
+            console.log("working from index 0");
+            var count = 0;
+            var current = this.head;
 
-        while(count != index){
-            current = current.next;
-            count++;
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+            return current;
+        } else {
+            console.log("working from last index");
+            var count = this.length -1;
+            var current = this.tail;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+            return current;
         }
-        return current;
     }
 }
 
 
 let list = new DoublyLinkedList();
-list.push("last");
-list.push("actually last");
+list.push("100");
+list.push("200");
+list.push("400");
+list.push("800");
+list.push("1600");
+list.push("3200");
 
 
-console.log(list.get(1));
-// {
-//     val: 'last',
-//     next: Node { val: 'actually last', next: null, prev: [Circular *1] },
-//     prev: null
+console.log(list.get(4));
+// OUTPUT:
+// working from last index
+// <ref *1> Node {
+//   val: '1600',
+//   next: Node { val: '3200', next: null, prev: [Circular *1] },
+//   prev: <ref *2> Node {
+//     val: '800',
+//     next: [Circular *1],
+//     prev: Node { val: '400', next: [Circular *2], prev: [Node] }
 //   }
+// }
--- dllSet.js
+++ dllSet.js
@@ -8,8 +8,9 @@ class Node{
 
 class DoublyLinkedList{
     constructor(val){
-        this.val = val
-        this.next = null;      
+        this.head = null;
+        this.tail = null;  
+        this.length = 0;    
     }
     push(val){
         var node = new Node(val);
@@ -24,7 +25,59 @@ class DoublyLinkedList{
         this.length++;
         return this;
     }
-    set(){
-        
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        if(index <= this.length/2){
+            var count = 0;
+            var current = this.head;
+
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+            return current;
+        } else {
+            var count = this.length -1;
+            var current = this.tail;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+            return current;
+        }
+    }
+    set(index, val){
+        var foundNode = this.get(index);
+        if (foundNode != null) {
+            foundNode.val = val;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+
+
+let list = new DoublyLinkedList();
+list.push("100");
+list.push("200");
+list.push("400");
+list.push("800");
+list.push("1600");
+list.push("3200");
+
+list.set(0, "444")
+console.log(list);
+// {
+//     head: <ref *1> Node {
+//       val: '444',
+//       next: Node { val: '200', next: [Node], prev: [Circular *1] },
+//       prev: null
+//     },
+//     tail: <ref *2> Node {
+//       val: '3200',
+//       next: null,
+//       prev: Node { val: '1600', next: [Circular *2], prev: [Node] }
+//     },
+//     length: 6
+//   }
