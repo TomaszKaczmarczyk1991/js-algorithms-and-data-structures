efaffd64a37eaa98f5019116b81f27fc8c2b4fed
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2024-05-13 13:19:46 +0200

    add solution

--- dllGet.js
+++ dllGet.js
@@ -8,8 +8,9 @@ class Node{
 
 class DoublyLinkedList{
     constructor(val){
-        this.val = val
-        this.next = null;      
+        this.head = null;
+        this.tail = null;  
+        this.length = 0;    
     }
     push(val){
         var node = new Node(val);
@@ -24,7 +25,28 @@ class DoublyLinkedList{
         this.length++;
         return this;
     }
-    get(){
-        
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var count = 0;
+        var current = this.head;
+
+        while(count != index){
+            current = current.next;
+            count++;
+        }
+        return current;
     }
 }
+
+
+let list = new DoublyLinkedList();
+list.push("last");
+list.push("actually last");
+
+
+console.log(list.get(1));
+// {
+//     val: 'last',
+//     next: Node { val: 'actually last', next: null, prev: [Circular *1] },
+//     prev: null
+//   }
